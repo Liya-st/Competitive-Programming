857666cb81c222e7a7b947a9a8f3d4e18efd11f2
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2025-04-09 11:01:54 +0300

    Time: 0 ms (100%), Space: 18.5 MB (9.44%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.py
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -1,17 +1,14 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        visited = [False]*len(rooms)
-        visited[0] = True
-        graph = defaultdict(list)
-        stack = [0]
-
-        while stack:
-            node = stack.pop()
-            for n in rooms[node]:
-                if not visited[n]:
-                    visited[n] = True
-                    stack.append(n)
-        return all(visited)
+        visited = set([0])
+        def dfs(node):
+            visited.add(node)
+            for neigh in rooms[node]:
+                if neigh not in visited:
+                    dfs(neigh)
+        dfs(0)
+        return len(rooms) == len(visited)
+        
 
         
 
