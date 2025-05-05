1614eb1e962ef918990605f18460b25ce1ba95e9
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2025-05-05 16:59:34 +0300

    Time: 7 ms (57.77%), Space: 18.9 MB (83.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,20 +1,45 @@
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.size = [1] * size
+    def find(self, node):
+        root = node
+        while self.root[root] != root:
+            root = self.root[root]
+        while node != root:
+            parent = self.root[node]
+            self.root[node] = root
+            node = parent
+        return root
+
+    def Union(self, node1, node2):
+        root1 = self.find(node1)
+        root2 = self.find(node2)
+
+        if root1 != root2:
+            if self.size[root1] > self.size[root2]:
+                self.size[root1] += self.size[root2]
+                self.root[root2] = root1
+            else:
+                self.size[root2] += self.size[root1]
+                self.root[root1] = root2
+
 class Solution:
     def findCircleNum(self, grid: List[List[int]]) -> int:
         visited = [False] * len(grid)
-        res = 0
+        res = len(grid)
+        ufd = UnionFind(len(grid))
 
-        def bfs(q):
-            while q:
-                node = q.popleft()
-                for i in range(len(grid)):
-                    if grid[node][i] == 1 and not visited[i]:
-                        q.append(i)
-                        visited[i] = True
         for i in range(len(grid)):
-            if not visited[i]:
-                bfs(deque([i]))
-                res +=1
+            for j in range(i+1, len(grid)):
+                if grid[i][j] == 1 and ufd.find(i) != ufd.find(j):
+                    res -=1
+                    ufd.Union(i, j)
+
         return res
+    
+
+
 
         
         
