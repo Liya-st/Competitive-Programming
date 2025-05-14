6ab02c57cb33dca782fbbf169734b354a2868161
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2025-05-14 17:24:14 +0300

    Time: 0 ms (100%), Space: 18.1 MB (21.07%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # take or no take approach
-        subsets = []
-        temp = []
-        def backtrack(i, temp):
-            if i >= len(nums):
-                subsets.append(temp[:])
-                return
-
-            temp.append(nums[i])
-            backtrack(i+1, temp)
-            temp.pop()
-            backtrack(i+1, temp)
-        backtrack(0, temp)
-        return subsets
+        res = []
+        for i in range(1 << len(nums)):
+            temp = []
+            for j, k in enumerate(bin(i)[2:].zfill(len(nums))):
+                if k == '1':
+                    temp.append(nums[j])
+            res.append(temp)
+        return res
+        
 
         
