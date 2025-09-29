711771f794fa460cc6ced3992eea8956ff3eb4cf
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2025-09-29 12:15:52 +0300

    Time: 234 ms (56.14%), Space: 17.8 MB (91.79%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,19 +1,9 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        memo = {}
-        def dp(rem, i):
-            ways = 0
-            if rem == 0:
-                return 1
-            if rem < 0 or i >= len(coins):
-                return 0
-            state = (rem , i)
-            if state in memo:
-                return memo[state]
-            if coins[i] <= rem:
-                ways += dp(rem - coins[i], i)
-            ways += dp(rem, i + 1)
-            memo[state] = ways
-            return memo[state]
-
-        return dp(amount, 0)
+        dp = [0] *( amount + 1)
+        dp[0] = 1
+        for coin in coins:
+            for i in range(1, amount + 1):
+                if i >= coin:
+                    dp[i] += dp[i - coin]
+        return dp[amount]
