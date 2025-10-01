53a27d31f63edabc6c0e66be6298bfb9f22c48bf
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2025-10-01 10:17:46 +0300

    Time: 400 ms (89.59%), Space: 42.9 MB (57.48%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,18 +1,12 @@
 class Solution:
     def longestCommonSubsequence(self, s: str, t: str) -> int:
-        memo = [[-1] * (len(t) + 1 )for _ in range(len(s) + 1)]
-        def dp(s_idx, t_idx):
-            arr = []
-            if s_idx >= len(s) or t_idx >= len(t):
-                return 0
-            
-            if memo[s_idx][t_idx] != -1:
-                return memo[s_idx][t_idx]
-            if s[s_idx] == t[t_idx]:
-                return 1 + dp(s_idx + 1, t_idx + 1)
-            else:
-                c1 = dp(s_idx + 1, t_idx)
-                c2 = dp(s_idx, t_idx + 1)
-            memo[s_idx][t_idx] = max(c1, c2)
-            return  memo[s_idx][t_idx]
-        return dp(0, 0)
+        dp = [[0]*(len(t) + 1) for _ in range(len(s) + 1)]
+
+        for i in range(len(s)-1, -1, -1):
+            for j in range(len(t)-1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        return dp[0][0]
+        
