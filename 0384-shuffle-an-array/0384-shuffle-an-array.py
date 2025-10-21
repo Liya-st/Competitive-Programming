03c930fd4b5352abdb0760e8193b1fd0c843111c
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2025-10-21 08:25:13 +0300

    Time: 20 ms (84.05%), Space: 21.8 MB (79.63%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.py
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -1,19 +1,15 @@
 class Solution:
 
     def __init__(self, nums: List[int]):
-        self.nums = list(nums)
-        self.original = list(nums)    
+        self.nums = nums
+        self.original = nums[:]   
 
     def reset(self) -> List[int]:
         return self.original
 
     def shuffle(self) -> List[int]:
         c = len(self.nums)
-        while c >= 0:
-            idx1 = random.choice(list(range(len(self.nums))))
-            idx2 = random.choice(list(range(len(self.nums))))
-            self.nums[idx1], self.nums[idx2] = self.nums[idx2], self.nums[idx1]
-            c-=1
+        random.shuffle(self.nums)
         return self.nums
 
         
