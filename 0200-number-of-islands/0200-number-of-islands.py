fae3a116863f632432844993d4a7bf7c40911faa
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2025-10-22 10:24:28 +0300

    Time: 250 ms (51.27%), Space: 20.4 MB (38.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,35 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        offset = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
-        res = 0
-        def inbound(row, col):
-            return (0 <= row < len(grid) and 0 <= col < len(grid[0]) ) and grid[row][col] == "1"
-            
+        m = len(grid)
+        n = len(grid[0])
+
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        visited = [[False]* n for _ in range(m)]
+
+        def inbound(i, j):
+            return (0 <= i < m) and (0 <= j < n)
 
         def dfs(i, j):
-            visited[i][j] = True
-            for row, col in offset:
-                new_i = i + row
-                new_j = j + col
-                if inbound(new_i, new_j) and not visited[new_i][new_j]:
-                    dfs(new_i, new_j)
-        for  i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if not visited[i][j] and grid[i][j] == "1":
-                    dfs(i, j)
+            if not inbound(i, j):
+                return 0
+
+            for x, y in directions:
+                x += i
+                y += j
+                if inbound(x, y) and grid[x][y] == "1":
+                    visited[x][y] = True
+                    grid[x][y] = "0"
+
+                    dfs(x, y)
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    grid[i][j] = "0"
                     res +=1
+                    dfs(i, j)
         return res
-        
+            
+
 
         
