ff273ea1508643a8476cd01a4e8c69c0376a4df9
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2025-11-16 11:50:03 +0300

    Time: 15 ms (66.67%), Space: 18 MB (97.75%) - LeetHub

---
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        res, temp = 0, 0
+        for i in range(len(s)):
+            if s[i] == "0":
+                res += temp * (temp + 1) // 2
+                temp = 0
+            else:
+                temp += 1
+
+        res += temp * (temp + 1) // 2
+        return res % (10**9 + 7)
